8d4dd3f76e14c08cf8c0c5531e56bd5142a35bd1
Nevil George <nevilgeorge2016@u.northwestern.edu>
2016-01-26 19:43:07 -0600

    working and cleaned naive script, need to write tests now

--- correct/src/checker.rs
+++ correct/src/checker.rs
@@ -22,6 +22,7 @@ pub fn add_deletions(set: &mut HashSet<String>, word: String) -> Vec<String> {
     let mut sub_word: String;
     let mut newly_added = vec![];
 
+    // delete one character at a time from every position
     for i in 0..word.len() {
         sub_word = (&word[..i]).to_string() + &word[i + 1..];
         set.insert(sub_word.clone());
@@ -36,6 +37,7 @@ pub fn add_insertions(set: &mut HashSet<String>, word: String) -> Vec<String> {
     let mut sub_word: String;
     let mut newly_added = vec![];
 
+    // insert every letter in the alphabet at every possible position
     for i in 0..word.len() {
         for c in alphabet.chars() {
             sub_word = (&word[..i]).to_string() + &c.to_string() + &word[i..];
@@ -58,6 +60,7 @@ pub fn add_replacements(set: &mut HashSet<String>, word: String) -> Vec<String> 
     let mut sub_word: String;
     let mut newly_added = vec![];
 
+    // replace a single letter with every alphabet
     for i in 0..word.len() {
         for c in alphabet.chars() {
             sub_word = (&word[..i]).to_string() + &c.to_string() + &word[i + 1..];
@@ -70,10 +73,10 @@ pub fn add_replacements(set: &mut HashSet<String>, word: String) -> Vec<String> 
 }
 
 pub fn add_transpositions(set: &mut HashSet<String>, word: String) -> Vec<String> {
-
     let mut sub_word: String;
     let mut newly_added = vec![];
 
+    // fail early if the word is just an empty string
     if word.len() == 0 {
         return newly_added;
     }
@@ -84,7 +87,7 @@ pub fn add_transpositions(set: &mut HashSet<String>, word: String) -> Vec<String
     }
 
     for i in 0..word.len() - 1 as usize {
-        // swap i and i + 1
+        // swap i and i + 1 letters
         sub_word = (&word[..i]).to_string();
         sub_word.push(characters[i + (1 as usize)]);
         sub_word.push(characters[i]);
@@ -97,6 +100,7 @@ pub fn add_transpositions(set: &mut HashSet<String>, word: String) -> Vec<String
 }
 
 pub fn find_most_likely_word(possible_words: HashSet<String>, corpus: &CountTable) -> String {
+    // keep track of max_value and word with max_value
     let mut max_word: String = "-".to_string();
     let mut max_value: usize = 0;
 
--- correct/src/main.rs
+++ correct/src/main.rs
@@ -19,48 +19,45 @@ fn main() {
     let mut possible_words: HashSet<String>;
     let mut newly_added: Vec<String>;
 
-    // add all possible deletions
     for word in words {
         possible_words = HashSet::<String>::new();
 
+        // add all possibe deletions, then perform all edits again
         newly_added = checker::add_deletions(&mut possible_words, word.clone());
-        for new_word in newly_added {
-            checker::add_deletions(&mut possible_words, new_word.clone());
-            checker::add_insertions(&mut possible_words, new_word.clone());
-            checker::add_replacements(&mut possible_words, new_word.clone());
-            checker::add_transpositions(&mut possible_words, new_word.clone());
-        }
+        run_all_edits(&mut possible_words, newly_added);
 
+        // add all possibe insertions, then perform all edits again
         newly_added = checker::add_insertions(&mut possible_words, word.clone());
-        for new_word in newly_added {
-            checker::add_deletions(&mut possible_words, new_word.clone());
-            checker::add_insertions(&mut possible_words, new_word.clone());
-            checker::add_replacements(&mut possible_words, new_word.clone());
-            checker::add_transpositions(&mut possible_words, new_word.clone());
-        }
+        run_all_edits(&mut possible_words, newly_added);
 
+        // add all possibe replacements, then perform all edits again
         newly_added = checker::add_replacements(&mut possible_words, word.clone());
-        for new_word in newly_added {
-            checker::add_deletions(&mut possible_words, new_word.clone());
-            checker::add_insertions(&mut possible_words, new_word.clone());
-            checker::add_replacements(&mut possible_words, new_word.clone());
-            checker::add_transpositions(&mut possible_words, new_word.clone());
-        }
+        run_all_edits(&mut possible_words, newly_added);
 
+        // add all possibe transpositions   , then perform all edits again
         newly_added = checker::add_transpositions(&mut possible_words, word.clone());
-        for new_word in newly_added {
-            checker::add_deletions(&mut possible_words, new_word.clone());
-            checker::add_insertions(&mut possible_words, new_word.clone());
-            checker::add_replacements(&mut possible_words, new_word.clone());
-            checker::add_transpositions(&mut possible_words, new_word.clone());
-        }
+        run_all_edits(&mut possible_words, newly_added);
 
+        // find a correct word
         let correction = checker::find_most_likely_word(possible_words, &corpus);
+        print_results(word, correction);
+    }
+}
 
-        if word == correction {
-            println!("{}", word);
-        } else {
-            println!("{}, {}", word, correction);
-        }
+fn run_all_edits(mut set: &mut HashSet<String>, words: Vec<String>) {
+    for word in words {
+        checker::add_deletions(&mut set, word.clone());
+        checker::add_insertions(&mut set, word.clone());
+        checker::add_replacements(&mut set, word.clone());
+        checker::add_transpositions(&mut set, word);
+    }
+}
+
+fn print_results(word: String, correction: String) {
+    // print only one word if there word was spelled correctly
+    if word == correction {
+        println!("{}", word);
+    } else {
+        println!("{}, {}", word, correction);
     }
 }
