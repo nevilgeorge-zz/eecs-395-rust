a8e4f4ec33ee77297c49f87f95a7ebe0409c1847
Nevil George <nevilgeorge2016@u.northwestern.edu>
2016-01-26 21:32:31 -0600

    checker tests finished

--- correct/src/checker_tests.rs
+++ correct/src/checker_tests.rs
@@ -56,11 +56,12 @@ mod read_words_tests {
     }
 }
 
-mod add_deletions_tests {
-    use checker::add_deletions;
+// functions that are common to all edit tests
+#[cfg(test)]
+mod edit_tests {
     use std::collections::HashSet;
 
-    fn initialize() -> HashSet<String> {
+    pub fn initialize_set() -> HashSet<String> {
         let mut set = HashSet::<String>::new();
         assert_eq!(set.len(), 0);
         // add random words to make set non empty
@@ -78,16 +79,23 @@ mod add_deletions_tests {
 
         set
     }
+}
+
+#[cfg(test)]
+mod add_deletions_tests {
+    use checker::add_deletions;
+    use super::edit_tests::initialize_set;
 
     #[test]
     fn add_deletions_string() {
         let word = "nevil".to_owned();
-        let mut set = initialize();
+        let mut set = initialize_set();
+        let set_size = set.len();
 
         let newly_added = add_deletions(&mut set, word);
 
         assert_eq!(newly_added.len(), 5);
-        assert_eq!(set.len(), 8);
+        assert_eq!(set.len(), set_size + newly_added.len());
 
         for new_word in newly_added {
             assert_eq!(set.contains(&new_word), true);
@@ -97,11 +105,118 @@ mod add_deletions_tests {
     #[test]
     fn add_deletions_empty_string() {
         let word = "".to_owned();
-        let mut set = initialize();
+        let mut set = initialize_set();
 
         let newly_added = add_deletions(&mut set, word);
 
         assert_eq!(newly_added.len(), 0);
         assert_eq!(set.len(), 3);
+    }
+}
+
+#[cfg(test)]
+mod add_insertions_tests {
+    use checker::add_insertions;
+    use super::edit_tests::initialize_set;
+
+    #[test]
+    fn add_insertions_string() {
+        let word = "nevil".to_owned();
+        let mut set = initialize_set();
+
+        let newly_added = add_insertions(&mut set, word.clone());
+
+        // 26 characters in alphabet being added to every position in 5 letter word
+        assert_eq!(newly_added.len(), 26 * (word.len() + 1));
+
+        for new_word in newly_added {
+            assert_eq!(set.contains(&new_word), true);
+        }
+    }
+
+    #[test]
+    fn add_insertions_empty_string() {
+        let word = "".to_owned();
+        let mut set = initialize_set();
+        let set_size = set.len();
+
+        let newly_added = add_insertions(&mut set, word);
+
+        // add 26 alphabets
+        assert_eq!(newly_added.len(), 26);
+        assert_eq!(set.len(), set_size + 26);
+
+        for new_word in newly_added {
+            assert_eq!(set.contains(&new_word), true);
+        }
+    }
+}
+
+#[cfg(test)]
+mod add_replacement_tests {
+    use checker::add_replacements;
+    use super::edit_tests::initialize_set;
+
+    #[test]
+    fn add_replacements_string() {
+        let word = "nevil".to_owned();
+        let mut set = initialize_set();
+
+        let newly_added = add_replacements(&mut set, word.clone());
+
+        // 26 characters in alphabet replacing every position in 5 letter word
+        assert_eq!(newly_added.len(), 26 * word.len());
+
+        for new_word in newly_added {
+            assert_eq!(set.contains(&new_word), true);
+        }
+    }
+
+    #[test]
+    fn add_replacements_empty_string() {
+        let word = "".to_owned();
+        let mut set = initialize_set();
+        let set_size = set.len();
+
+        let newly_added = add_replacements(&mut set, word);
+
+        // nothing to replace so nothing added
+        assert_eq!(newly_added.len(), 0);
+        assert_eq!(set.len(), set_size);
+    }
+}
+
+#[cfg(test)]
+mod add_transpositions_tests {
+    use checker::add_transpositions;
+    use super::edit_tests::initialize_set;
+
+    #[test]
+    fn add_transpositions_string() {
+        let word = "nevil".to_owned();
+        let mut set = initialize_set();
+        let set_size = set.len();
+
+        let newly_added = add_transpositions(&mut set, word.clone());
+
+        assert_eq!(newly_added.len(), word.len() - 1);
+        assert_eq!(set.len(), set_size + newly_added.len());
+
+        for new_word in newly_added {
+            assert_eq!(set.contains(&new_word), true);
+        }
+    }
+
+    #[test]
+    fn add_transpositions_empty_string() {
+        let word = "".to_owned();
+        let mut set = initialize_set();
+        let set_size = set.len();
+
+        let newly_added = add_transpositions(&mut set, word);
+
+        // nothing to swap so nothing added
+        assert_eq!(newly_added.len(), 0);
+        assert_eq!(set.len(), set_size);
     }
 }
