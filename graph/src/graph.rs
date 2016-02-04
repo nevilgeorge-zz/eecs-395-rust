732b453d2db8d5a9f71bbc110b74f2b3801c8936
Diane Liu <dianeliu@u.northwestern.edu>
2016-02-04 16:00:09 -0600

    graph daata struct, add node, add edge

--- graph/src/graph.rs
+++ graph/src/graph.rs
@@ -1,1 +1,28 @@
 // graph.rs
+
+use std::collections::{HashMap, HashSet};
+
+pub struct Graph {
+    graph: HashMap<String, HashSet<String>>,
+}
+
+impl Graph {
+    pub fn new() -> Self {
+        Graph {
+            graph: HashMap::<String, HashSet<String>>::new(),
+        }
+    }
+
+    pub fn add_node(&mut self, node: String) {
+        self.graph.insert(node.to_owned(), HashSet::<String>::new());
+    }
+
+    pub fn add_edge(&mut self, node_a: String, node_b: String) {
+        if let Some(set) = self.graph.get_mut(&node_a) {
+            set.insert(node_b.to_owned());
+        }
+        if let Some(set) = self.graph.get_mut(&node_b) {
+            set.insert(node_a.to_owned());
+        }
+    }
+}
