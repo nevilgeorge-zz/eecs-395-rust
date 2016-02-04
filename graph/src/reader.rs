e86eed3e888455fd2131967845c0d70e4454b5e5
Diane Liu <dianeliu@u.northwestern.edu>
2016-02-04 15:46:47 -0600

    redo graph

--- graph/src/reader.rs
+++ graph/src/reader.rs
@@ -1,6 +1,6 @@
 // reader.rs
 
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Read};
 
@@ -14,7 +14,7 @@ pub fn read_query<R: Read>(reader: R) -> Vec<(String, String)> {
 
             let (size, _) = tokens.size_hint();
             if size > (2 as usize) {
-                println!("{}", "Too many nodes entered!");
+                println!("Too many nodes entered!");
                 break;
             }
 
@@ -26,27 +26,27 @@ pub fn read_query<R: Read>(reader: R) -> Vec<(String, String)> {
     nodes
 }
 
-pub fn read_graph(filename: &str) -> HashMap<String, HashSet<String>> {
-
-    let mut mapping = HashMap::<String, HashSet<String>>::new();
+pub fn read_graph(filename: &str) -> HashMap<String, Vec<String>> {
+    let mut mapping = HashMap::<String, Vec<String>>::new();
     let f = File::open(filename).expect("Error opening graph description file!");
     let lines = BufReader::new(&f).lines();
-    let mut adjacent_nodes: HashSet<String>;
+    let mut adjacent_nodes: Vec<String>;
 
     for line in lines {
         if let Ok(text) = line {
             let mut tokens = text.trim().split(" ");
             let key = tokens.next();
 
+            // check there are no duplicate nodes in input file
             if let Some(node_key) = key {
                 if mapping.contains_key(node_key) {
                     println!("Graph cannot have duplicate nodes!");
                     break;
                 }
 
-                adjacent_nodes = HashSet::<String>::new();
+                adjacent_nodes = vec![];
                 for token in tokens {
-                    adjacent_nodes.insert(token.to_owned());
+                    adjacent_nodes.push(token.to_owned());
                 }
 
                 mapping.insert(node_key.to_owned(), adjacent_nodes);
@@ -55,23 +55,5 @@ pub fn read_graph(filename: &str) -> HashMap<String, HashSet<String>> {
             }
         }
     }
-
-    create_full_graph(&mut mapping);
     mapping
-}
-
-fn create_full_graph(mapping: &mut HashMap<String, HashSet<String>>) {
-    for (node, neighbors) in mapping {
-        for neighbor in neighbors.iter() {
-            println!("{}", neighbor);
-            // let set = mapping.get_mut(&neighbor).unwrap();
-            let mut set = mapping.get_mut(neighbor).unwrap();
-
-            // *set.insert(node);
-            // new_mapping.insert(node, *set);
-            // for item in set.iter() {
-            //     println!("{}", item);
-            // }
-        }
-    }
 }
