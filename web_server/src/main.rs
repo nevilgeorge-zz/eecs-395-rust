45e80a27c91db8a50678b45eb3bf464f4285d511
Nevil George <nevilgeorge2016@u.northwestern.edu>
2016-02-10 19:19:43 -0600

    basic server working, reading from connection

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -1,3 +1,26 @@
+// main.rs
+use std::io::Read;
+use std::net::TcpListener;
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    println!("Ready to accept connections on port 8080.");
+
+    for stream in listener.incoming() {
+        // spawn new thread for new connection
+        thread::spawn(|| {
+            println!("New connection!");
+            // grab TcpStream
+            let mut stream = stream.unwrap();
+            let mut buf = [0; 128];
+            // pass in a buffer and read from connection
+            let reading = stream.read(&mut buf[..]);
+            if let Ok(_) = reading {
+                // convert utf8 buffer to string
+                let line = String::from_utf8(buf.to_owned()).unwrap();
+                println!("{}", line);
+            }
+        });
+    }
 }
