76cde911da63f7475f2fca9e6714a5fda83a028c
Nevil George <nevilgeorge2016@u.northwestern.edu>
2016-02-10 21:46:21 -0600

    input parsing function implemented

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -3,6 +3,12 @@ use std::io::Read;
 use std::net::TcpListener;
 use std::thread;
 
+struct Request {
+    method: String,
+    file_path: String,
+    protocol: String,
+}
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
     println!("Ready to accept connections on port 8080.");
@@ -18,9 +24,32 @@ fn main() {
             let reading = stream.read(&mut buf[..]);
             if let Ok(_) = reading {
                 // convert utf8 buffer to string
-                let line = String::from_utf8(buf.to_owned()).unwrap();
-                println!("{}", line);
+                let input_line = String::from_utf8(buf.to_owned()).unwrap();
+                let result = parse_input(input_line);
+                match result {
+                    Ok(request) => {
+                        println!("Correct request received!");
+                    },
+                    Err(e) => {
+                        println!("{}", e);
+                    }
+                }
             }
         });
     }
+}
+
+fn parse_input(input: String) -> Result<Request, String> {
+    let tokens: Vec<&str> = input.split(" ").collect();
+    if tokens.len() != 3 || tokens[0] != "GET" || !tokens[2].contains("HTTP")  {
+        return Err("400 Bad Request".to_owned());
+    }
+
+    let request = Request {
+        method: tokens[0].to_string(),
+        file_path: tokens[1].to_string(),
+        protocol: tokens[2].to_string(),
+    };
+
+    Ok(request)
 }
